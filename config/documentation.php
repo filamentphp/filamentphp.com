74274f963745ea8d77280b7b6e87b183a2035311
Dan Harrin <git@danharrin.com>
2021-09-15 20:29:32 +0100

    Update documentation.php

--- config/documentation.php
+++ config/documentation.php
@@ -20,41 +20,41 @@ return [
 
     ],
 
-    2 => [
-
-        'packages' => [
-
-            'forms' => [
-
-                'name' => 'Form Builder',
-
-                'description' => 'The elegant TALL stack form builder for Laravel artisans.',
-
-                'path' => resource_path('filament/2.x/packages/forms/docs'),
-
-            ],
-
-            'spatie-laravel-media-library-plugin' => [
-
-                'name' => 'spatie/laravel-medialibrary Plugin',
-
-                'description' => 'A plugin to add support for spatie/laravel-medialibrary to Filament.',
-
-                'path' => resource_path('filament/2.x/packages/spatie-laravel-media-library-plugin/docs'),
-
-            ],
-
-            'spatie-laravel-tags-plugin' => [
-
-                'name' => 'spatie/laravel-tags Plugin',
-
-                'description' => 'A plugin to add support for spatie/laravel-medialibrary to Filament.',
-
-                'path' => resource_path('filament/2.x/packages/spatie-laravel-tags-plugin/docs'),
-
-            ],
-        ],
-
-    ],
+//    2 => [
+//
+//        'packages' => [
+//
+//            'forms' => [
+//
+//                'name' => 'Form Builder',
+//
+//                'description' => 'The elegant TALL stack form builder for Laravel artisans.',
+//
+//                'path' => resource_path('filament/2.x/packages/forms/docs'),
+//
+//            ],
+//
+//            'spatie-laravel-media-library-plugin' => [
+//
+//                'name' => 'spatie/laravel-medialibrary Plugin',
+//
+//                'description' => 'A plugin to add support for spatie/laravel-medialibrary to Filament.',
+//
+//                'path' => resource_path('filament/2.x/packages/spatie-laravel-media-library-plugin/docs'),
+//
+//            ],
+//
+//            'spatie-laravel-tags-plugin' => [
+//
+//                'name' => 'spatie/laravel-tags Plugin',
+//
+//                'description' => 'A plugin to add support for spatie/laravel-medialibrary to Filament.',
+//
+//                'path' => resource_path('filament/2.x/packages/spatie-laravel-tags-plugin/docs'),
+//
+//            ],
+//        ],
+//
+//    ],
 
 ];
