464bbe50d15be86c2a138019216730d55de46ec3
Dan Harrin <git@danharrin.com>
2021-09-16 07:48:50 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -19,69 +19,79 @@ Route::view('/', 'home')->name('home');
 
 Route::redirect('/discord', 'https://discord.gg/cpqnMTHZja');
 
-Route::get('/docs/{versionSlug?}/{packageSlug?}/{pageSlug?}', function ($versionSlug = null, ?string $packageSlug = null, ?string $pageSlug = null) {
-    // Get requested version
-    $version = DocumentationVersion::where('slug', $versionSlug)->first();
+Route::prefix('/docs')->group(function () {
+    Route::redirect('/getting-started', '/docs/admin/getting-started');
+    Route::redirect('/resources', '/docs/admin/resources');
+    Route::redirect('/pages', '/docs/admin/pages');
+    Route::redirect('/dashboard', '/docs/admin/dashboard');
+    Route::redirect('/navigation', '/docs/admin/navigation');
+    Route::redirect('/theming', '/docs/admin/theming');
+    Route::redirect('/plugin-development', '/docs/admin/plugin-development');
 
-    // If version doesn't exist
-    if (! $version) {
-        // Check if docs have been requested without a version number
-        if ($package = DocumentationPackage::orderByDesc('version_id')->where('slug', $versionSlug)->first()) {
+    Route::get('/{versionSlug?}/{packageSlug?}/{pageSlug?}', function ($versionSlug = null, ?string $packageSlug = null, ?string $pageSlug = null) {
+        // Get requested version
+        $version = DocumentationVersion::where('slug', $versionSlug)->first();
+
+        // If version doesn't exist
+        if (! $version) {
+            // Check if docs have been requested without a version number
+            if ($package = DocumentationPackage::orderByDesc('version_id')->where('slug', $versionSlug)->first()) {
+                return redirect()->route('docs', [
+                    'packageSlug' => $package->slug,
+                    'pageSlug' => $packageSlug,
+                    'versionSlug' => $package->version,
+                ]);
+            }
+
+            // Use latest version
+            return redirect()->route('docs', [
+                'packageSlug' => $packageSlug,
+                'pageSlug' => $pageSlug,
+                'versionSlug' => DocumentationVersion::orderByDesc('id')->first(),
+            ]);
+        }
+
+        // Get requested package
+        $package = $version->packages()->where('slug', $packageSlug)->first();
+
+        // If package doesn't exist with this version, use latest existing version or another package at this version
+        if (! $package) {
+            $package = DocumentationPackage::orderByDesc('version_id')->where('slug', $packageSlug)->first() ?? $version->packages()->first();
+
             return redirect()->route('docs', [
                 'packageSlug' => $package->slug,
-                'pageSlug' => $packageSlug,
+                'pageSlug' => $pageSlug,
                 'versionSlug' => $package->version,
             ]);
         }
 
-        // Use latest version
-        return redirect()->route('docs', [
-            'packageSlug' => $packageSlug,
-            'pageSlug' => $pageSlug,
-            'versionSlug' => DocumentationVersion::orderByDesc('id')->first(),
+        // Get requested page
+        $page = $package->getPage($pageSlug);
+
+        // If page doesn't exist in this package, use first page
+        if (! $page) {
+            return redirect()->route('docs', [
+                'packageSlug' => $package->slug,
+                'pageSlug' => $package->getPages()->first()->slug,
+                'versionSlug' => $version,
+            ]);
+        }
+
+        seo()
+            ->title("{$page->title} - {$package->name}")
+            ->description($package->description)
+            ->image(og()->generate([
+                'template' => '_og-image',
+                'title' => $page->title,
+                'description' => $package->description,
+                'package' => $package->name,
+                'packageVersionNumber' => $package->version_id,
+            ]));
+
+        return view('docs', [
+            'package' => $package,
+            'page' => $page,
+            'version' => $version,
         ]);
-    }
-
-    // Get requested package
-    $package = $version->packages()->where('slug', $packageSlug)->first();
-
-    // If package doesn't exist with this version, use latest existing version or another package at this version
-    if (! $package) {
-        $package = DocumentationPackage::orderByDesc('version_id')->where('slug', $packageSlug)->first() ?? $version->packages()->first();
-
-        return redirect()->route('docs', [
-            'packageSlug' => $package->slug,
-            'pageSlug' => $pageSlug,
-            'versionSlug' => $package->version,
-        ]);
-    }
-
-    // Get requested page
-    $page = $package->getPage($pageSlug);
-
-    // If page doesn't exist in this package, use first page
-    if (! $page) {
-        return redirect()->route('docs', [
-            'packageSlug' => $package->slug,
-            'pageSlug' => $package->getPages()->first()->slug,
-            'versionSlug' => $version,
-        ]);
-    }
-
-    seo()
-        ->title("{$page->title} - {$package->name}")
-        ->description($package->description)
-        ->image(og()->generate([
-            'template' => '_og-image',
-            'title' => $page->title,
-            'description' => $package->description,
-            'package' => $package->name,
-            'packageVersionNumber' => $package->version_id,
-        ]));
-
-    return view('docs', [
-        'package' => $package,
-        'page' => $page,
-        'version' => $version,
-    ]);
-})->name('docs');
+    })->name('docs');
+});
