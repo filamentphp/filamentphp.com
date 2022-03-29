d36bd900289c1230486c215c7bb5d11b19b74c0e
Dan Harrin <git@danharrin.com>
2022-03-29 08:55:16 +0100

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -31,6 +31,7 @@ class UserResource extends Resource
                     ->maxLength(255)
                     ->unique(ignoreRecord: true),
                 Forms\Components\Toggle::make('is_admin')->label('Admin'),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
             ]);
     }
 
