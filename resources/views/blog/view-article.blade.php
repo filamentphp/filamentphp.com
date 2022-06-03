d52167dec94c91036912f93c2bab4b13dbb4d047
Dan Harrin <git@danharrin.com>
2022-06-03 19:53:51 +0100

    Update view-article.blade.php

--- resources/views/blog/view-article.blade.php
+++ resources/views/blog/view-article.blade.php
@@ -81,6 +81,32 @@
                     @markdown($article->content)
                 </div>
 
+                <div class="text-sm flex items-center gap-3 w-full mt-8">
+                    <a
+                        href="https://twitter.com/share?url={{ $article->getUrl() }}&text={{ urlencode("📝 {$article->title} by {$article->author->name}") }}"
+                        target="__blank"
+                        class="flex items-center gap-2 text-gray-500 hover:text-gray-600"
+                    >
+                        <svg class="w-4 h-4 text-blue-400 hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                            <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
+                        </svg>
+
+                        <span>Share on Twitter</span>
+                    </a>
+
+                    <a
+                        href="/blog/feed"
+                        target="__blank"
+                        class="flex items-center gap-2 text-gray-500 hover:text-gray-600"
+                    >
+                        <svg class="w-4 h-4 text-orange-400 hover:text-orange-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                        </svg>
+
+                        <span>Subscribe via RSS</span>
+                    </a>
+                </div>
+
 {{--                <div aria-hidden="true" class="mt-8 border-t"></div>--}}
 
 {{--                <div class="mt-8 space-y-2">--}}
