65a9e8a577021abc3384c5c5263da4f8f5237d4b
Andréas Lundgren <adevade@gmail.com>
2022-10-02 20:54:02 +0200

    Get random plugin from most popular top row

--- app/Http/Controllers/Plugins/ListPluginsController.php
+++ app/Http/Controllers/Plugins/ListPluginsController.php
@@ -18,7 +18,9 @@ class ListPluginsController extends Controller
                 ->published()
                 ->with(['author', 'media'])
                 ->orderByDesc('views')
-                ->first(),
+                ->limit(4)
+                ->get()
+                ->random(),
             'featuredPlugins' => Plugin::query()
                 ->published()
                 ->with(['author', 'media'])
