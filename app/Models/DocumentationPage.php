cdc63f2fdeb115071494334aec2ef1369eeb08f9
Dan Harrin <git@danharrin.com>
2022-10-04 09:22:30 +0100

    Update DocumentationPage.php

--- app/Models/DocumentationPage.php
+++ app/Models/DocumentationPage.php
@@ -49,7 +49,7 @@ class DocumentationPage extends Model
                         'slug' => $slug,
                         'title' => $page->matter('title'),
                         'version_id' => $versionNumber,
-                        'path_name' => $file->getPathName()
+                        'path_name' => $file->getPathName(),
                     ];
                 }
             }
@@ -78,12 +78,11 @@ class DocumentationPage extends Model
 
     public function getGithubLink(): string 
     {
-        return  Str::of($this->path_name)
+        return Str::of($this->path_name)
             ->after(resource_path())
             ->after('filament')
             ->prepend('https://github.com/filamentphp/filament/edit');
     }
-
 
     public function getSectionUrl(): ?string
     {
