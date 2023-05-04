2f30933ea92934302a29124f495e02098d40018f
Zep Fietje <hey@zepfietje.com>
2023-05-04 15:26:35 +0200

    Update resources/views/tricks/view-trick.blade.php

--- resources/views/tricks/view-trick.blade.php
+++ resources/views/tricks/view-trick.blade.php
@@ -32,7 +32,7 @@
                             <dd>{{ $trick->author->name }}</dd>
                         </div>
 
-                        @if($trick->filament_version)
+                        @if ($trick->filament_version)
                             <div class="flex items-center gap-2">
                                 <dt>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
