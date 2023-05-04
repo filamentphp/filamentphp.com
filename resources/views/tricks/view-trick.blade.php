3bb3fdca966777814e2a7bb135bf1fed53d37636
Tony Partridge <admin@xtech.im>
2023-05-04 13:20:49 +0100

    Update view-trick.blade.php

--- resources/views/tricks/view-trick.blade.php
+++ resources/views/tricks/view-trick.blade.php
@@ -32,6 +32,18 @@
                             <dd>{{ $trick->author->name }}</dd>
                         </div>
 
+                        @if($trick->filament_version)
+                            <div class="flex items-center gap-2">
+                                <dt>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                    </svg>
+                                </dt>
+
+                                <dd>{{ $trick->filament_version }}</dd>
+                            </div>
+                        @endif
+                        
                         <div class="flex items-center gap-2">
                             <dt>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
