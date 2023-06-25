f6e49d81dbd029d7bef84c75c9d8499e61117dc5
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-06-25 16:48:48 +0330

    Fix vite server websocket error

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+    },
 });
