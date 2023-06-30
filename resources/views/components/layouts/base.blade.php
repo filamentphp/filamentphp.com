b2ff7fc334a784854db0730e7373a300514387ea
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-06-30 22:59:35 +0330

    Fix mixed content warning in browser

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -9,6 +9,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <meta
             name="application-name"
