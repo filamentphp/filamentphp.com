6fedce6ad6f9957b4297adfdeaf2ddd6f5cc1b97
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-06-30 21:19:31 +0330

    Add reveal animation to v3features

--- resources/views/components/home/v3features.blade.php
+++ resources/views/components/home/v3features.blade.php
@@ -7,6 +7,174 @@
         x-data="{}"
         x-init="
             $nextTick(() => {
+                gsap.timeline({
+                    delay: 0.5,
+                    scrollTrigger: {
+                        trigger: $refs.header,
+                        start: 'top bottom-=150px',
+                    },
+                })
+                    .fromTo(
+                        $refs.header_introducing,
+                        {
+                            autoAlpha: 0,
+                            y: -30,
+                        },
+                        {
+                            autoAlpha: 1,
+                            y: 0,
+                            duration: 0.7,
+                            ease: 'circ.out',
+                        },
+                    )
+                    .fromTo(
+                        $refs.header_new,
+                        {
+                            autoAlpha: 0,
+                            x: -30,
+                            y: 30,
+                        },
+                        {
+                            autoAlpha: 1,
+                            x: 0,
+                            y: 0,
+                            duration: 0.7,
+                            ease: 'circ.out',
+                        },
+                        '>-0.6',
+                    )
+                    .fromTo(
+                        $refs.header_version3,
+                        {
+                            autoAlpha: 0,
+                            y: 30,
+                        },
+                        {
+                            autoAlpha: 1,
+                            y: 0,
+                            duration: 0.7,
+                            ease: 'circ.out',
+                        },
+                        '>-0.6',
+                    )
+                    .fromTo(
+                        $refs.header_features,
+                        {
+                            autoAlpha: 0,
+                            x: 30,
+                            y: 30,
+                        },
+                        {
+                            autoAlpha: 1,
+                            x: 0,
+                            y: 0,
+                            duration: 0.7,
+                            ease: 'circ.out',
+                        },
+                        '>-0.6',
+                    )
+                gsap.fromTo(
+                    $refs.feature_1,
+                    {
+                        autoAlpha: 0,
+                        x: -20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_1,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
+                gsap.fromTo(
+                    $refs.feature_2,
+                    {
+                        autoAlpha: 0,
+                        x: 20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_2,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
+                gsap.fromTo(
+                    $refs.feature_3,
+                    {
+                        autoAlpha: 0,
+                        x: -20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_3,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
+                gsap.fromTo(
+                    $refs.feature_4,
+                    {
+                        autoAlpha: 0,
+                        x: 20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_4,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
+                gsap.fromTo(
+                    $refs.feature_5,
+                    {
+                        autoAlpha: 0,
+                        x: -20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_5,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
+                gsap.fromTo(
+                    $refs.feature_6,
+                    {
+                        autoAlpha: 0,
+                        x: 20,
+                    },
+                    {
+                        autoAlpha: 1,
+                        x: 0,
+                        duration: 0.7,
+                        ease: 'circ.out',
+                        scrollTrigger: {
+                            trigger: $refs.feature_6,
+                            start: 'top bottom-=150px',
+                        },
+                    },
+                )
                 gsap.to($refs.geometric_shape_1, {
                     yPercent: -400,
                     rotate: 100,
@@ -79,11 +247,31 @@
             x-ref="header"
             class="text-center"
         >
-            <div class="font-medium text-rum">Introducing The</div>
+            <div
+                x-ref="header_introducing"
+                class="font-medium text-rum"
+            >
+                Introducing The
+            </div>
             <div class="pt-2 text-2xl sm:text-3xl">
-                <span class="">New</span>
-                <span class="font-black">Version 3</span>
-                <span class="">Features!</span>
+                <span
+                    x-ref="header_new"
+                    class="inline-block"
+                >
+                    New
+                </span>
+                <span
+                    x-ref="header_version3"
+                    class="inline-block font-black"
+                >
+                    Version 3
+                </span>
+                <span
+                    x-ref="header_features"
+                    class="inline-block"
+                >
+                    Features!
+                </span>
             </div>
         </div>
         <div
@@ -315,7 +503,7 @@
                 x-ref="feature_5"
                 class="relative flex flex-wrap items-center justify-around gap-10 lg:justify-center lg:gap-x-32"
             >
-                <div class="absolute left-0 bottom-0 hidden lg:block">
+                <div class="absolute bottom-0 left-0 hidden lg:block">
                     <img
                         x-ref="geometric_shape_5"
                         src="{{ Vite::asset('resources/images/home/geometric-shape-5.webp') }}"
@@ -358,7 +546,10 @@
 
                     {{-- Description --}}
                     <div class="max-w-xs pt-3 font-medium text-rum">
-                        Build multiple completely separate Filament-powered apps with their own resources, dashboards, custom pages and configuration. Ship an entire application in a Composer package with ease.
+                        Build multiple completely separate Filament-powered apps
+                        with their own resources, dashboards, custom pages and
+                        configuration. Ship an entire application in a Composer
+                        package with ease.
                     </div>
                 </div>
             </div>
@@ -368,7 +559,7 @@
                 x-ref="feature_6"
                 class="relative flex flex-wrap items-center justify-around gap-10 lg:justify-center lg:gap-x-32"
             >
-                <div class="absolute right-10 -bottom-40 hidden lg:block">
+                <div class="absolute -bottom-40 right-10 hidden lg:block">
                     <img
                         x-ref="geometric_shape_6"
                         src="{{ Vite::asset('resources/images/home/geometric-shape-6.webp') }}"
@@ -411,7 +602,10 @@
 
                     {{-- Description --}}
                     <div class="max-w-xs pt-3 font-medium text-rum">
-                        Build multiple completely separate Filament-powered apps with their own resources, dashboards, custom pages and configuration. Ship an entire application in a Composer package with ease.
+                        Build multiple completely separate Filament-powered apps
+                        with their own resources, dashboards, custom pages and
+                        configuration. Ship an entire application in a Composer
+                        package with ease.
                     </div>
                 </div>
             </div>
