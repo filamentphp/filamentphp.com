59b8cdc2443bc040ad1ea0c89176b16d155ad8f9
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-07-08 00:47:42 +0330

    Make filters responsive

--- resources/views/components/plugins/list.blade.php
+++ resources/views/components/plugins/list.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="mx-auto w-full max-w-8xl px-10"
+    class="mx-auto w-full max-w-8xl px-5 sm:px-10"
     x-cloak
     x-data="{
         search: '',
@@ -90,26 +90,29 @@
                     (this.features.dark_mode ? plugin.features.dark_mode : true) &&
                     (this.features.multi_language
                         ? plugin.features.multi_language
-                        : true)
+                        : true) &&
                     // If the selectedPrice is 'All', show all plugins, if the selectedPrice is 'Free', only show plugins that have a price of 'Free', if the selectedPrice is 'Paid', only show plugins that have a price that is not 'Free'
-                    && (this.selectedPrice === 'All' || (this.selectedPrice === 'Free' && plugin.price === 'Free') || (this.selectedPrice === 'Paid' && plugin.price !== 'Free'))
+                    (this.selectedPrice === 'All' ||
+                        (this.selectedPrice === 'Free' &&
+                            plugin.price === 'Free') ||
+                        (this.selectedPrice === 'Paid' && plugin.price !== 'Free')),
             )
         },
     }"
 >
-    <div class="flex items-start gap-5 pt-5">
-        {{-- Version --}}
-        <div class="w-full max-w-[15rem]">
+    <div class="flex flex-wrap items-center gap-3 pt-5">
+        {{-- Version Switch --}}
+        <div class="min-[1170px]:min-w-[15rem]">
             <div
-                class="relative z-10 inline-flex select-none items-center gap-2.5 rounded-full bg-white p-[.55rem] font-medium shadow-lg shadow-black/[0.02]"
+                class="relative z-10 inline-flex select-none items-center gap-2.5 rounded-full bg-white p-[.55rem] font-medium shadow-lg shadow-black/[0.01]"
             >
                 <div
                     x-on:click="selectedVersion = '1'"
                     class="relative z-20 w-14 text-center transition duration-300"
                     :class="{
-                        'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '1',
-                        'text-salmon': selectedVersion === '1',
-                    }"
+                            'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '1',
+                            'text-salmon': selectedVersion === '1',
+                        }"
                 >
                     v1.x
                 </div>
@@ -117,9 +120,9 @@
                     class="relative z-20 w-14 text-center transition duration-300"
                     x-on:click="selectedVersion = '2'"
                     :class="{
-                        'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '2',
-                        'text-salmon': selectedVersion === '2',
-                    }"
+                            'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '2',
+                            'text-salmon': selectedVersion === '2',
+                        }"
                 >
                     v2.x
                 </div>
@@ -127,27 +130,26 @@
                     class="relative z-20 w-14 text-center transition duration-300"
                     x-on:click="selectedVersion = '3'"
                     :class="{
-                        'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '3',
-                        'text-salmon': selectedVersion === '3',
-                    }"
+                            'cursor-pointer opacity-50 hover:opacity-100': selectedVersion !== '3',
+                            'text-salmon': selectedVersion === '3',
+                        }"
                 >
                     v3.x
                 </div>
                 <div
                     class="absolute left-[.31rem] top-[.31rem] -z-10 h-8 w-16 rounded-full bg-fair-pink transition duration-300 ease-out will-change-transform"
                     :class="{
-                        'translate-x-[4.1rem]': selectedVersion === '2',
-                        'translate-x-[8.2rem]': selectedVersion === '3',
-                    }"
+                            'translate-x-[4.1rem]': selectedVersion === '2',
+                            'translate-x-[8.2rem]': selectedVersion === '3',
+                        }"
                 ></div>
             </div>
         </div>
 
-        {{-- Right Side --}}
-        <div class="flex w-full flex-wrap items-center justify-between gap-5">
-            {{-- Search Bar --}}
+        {{-- Search Bar --}}
+        <div class="w-full sm:w-auto min-[1170px]:flex-1">
             <div
-                class="group/search-bar relative w-full max-w-xs overflow-hidden rounded-full bg-white shadow-lg shadow-black/[0.02] transition duration-200 focus-within:shadow-xl focus-within:shadow-black/[0.03]"
+                class="group/search-bar relative w-full overflow-hidden rounded-full bg-white shadow-lg shadow-black/[0.01] transition duration-200 focus-within:shadow-xl focus-within:shadow-black/[0.03] sm:max-w-xs"
             >
                 {{-- Magnify Icon --}}
                 <div
@@ -170,7 +172,6 @@
                         />
                     </svg>
                 </div>
-
                 <!-- X icon -->
                 <div
                     class="absolute right-0 top-0 grid h-full w-12 place-items-center bg-transparent"
@@ -196,7 +197,6 @@
                         />
                     </svg>
                 </div>
-
                 {{-- Search Input --}}
                 <input
                     type="text"
@@ -205,131 +205,132 @@
                     class="w-full appearance-none border-none bg-transparent py-3 pl-12 pr-10 text-sm outline-none placeholder:transition placeholder:duration-200 focus:ring-0 group-focus-within/search-bar:placeholder:translate-x-1 group-focus-within/search-bar:placeholder:opacity-0"
                 />
             </div>
+        </div>
 
-            {{-- Features --}}
-            <div class="flex flex-wrap items-center gap-3">
-                {{-- Price Toggle --}}
-                <div
-                    class="relative z-10 inline-flex h-11 select-none items-center gap-2.5 rounded-full bg-white px-[.55rem] text-sm font-medium shadow-lg shadow-black/[0.02]"
-                >
-                    <div
-                        x-on:click="selectedPrice = 'All'"
-                        class="relative z-20 w-14 text-center transition duration-300"
-                        :class="{
+        {{-- Price Toggle --}}
+        <div
+            class="relative z-10 flex h-11 w-[205px] select-none items-center justify-between gap-1 rounded-full bg-white px-[.55rem] text-sm font-medium shadow-lg shadow-black/[0.01]"
+        >
+            <div
+                x-on:click="selectedPrice = 'All'"
+                class="relative z-20 w-14 text-center transition duration-300"
+                :class="{
                         'cursor-pointer opacity-50 hover:opacity-100': selectedPrice !== 'All',
                         'text-salmon': selectedPrice === 'All',
                     }"
-                    >
-                        All
-                    </div>
-                    <div
-                        x-on:click="selectedPrice = 'Free'"
-                        class="relative z-20 w-14 text-center transition duration-300"
-                        :class="{
+            >
+                All
+            </div>
+            <div
+                x-on:click="selectedPrice = 'Free'"
+                class="relative z-20 w-14 text-center transition duration-300"
+                :class="{
                         'cursor-pointer opacity-50 hover:opacity-100': selectedPrice !== 'Free',
                         'text-salmon': selectedPrice === 'Free',
                     }"
-                    >
-                        Free
-                    </div>
-                    <div
-                        x-on:click="selectedPrice = 'Paid'"
-                        class="relative z-20 w-14 text-center transition duration-300"
-                        :class="{
+            >
+                Free
+            </div>
+            <div
+                x-on:click="selectedPrice = 'Paid'"
+                class="relative z-20 w-14 text-center transition duration-300"
+                :class="{
                         'cursor-pointer opacity-50 hover:opacity-100': selectedPrice !== 'Paid',
                         'text-salmon': selectedPrice === 'Paid',
                     }"
-                    >
-                        Paid
-                    </div>
-                    <div
-                        class="absolute left-[.31rem] top-[.31rem] -z-10 h-8 w-16 rounded-full bg-fair-pink transition duration-300 ease-out will-change-transform"
-                        :class="{
-                        'translate-x-[4.1rem]': selectedPrice === 'Free',
+            >
+                Paid
+            </div>
+            <div
+                class="absolute left-[.35rem] top-[.35rem] -z-10 h-8 w-16 rounded-full bg-fair-pink transition duration-300 ease-out will-change-transform"
+                :class="{
+                        'translate-x-[4rem]': selectedPrice === 'Free',
                         'translate-x-[8.2rem]': selectedPrice === 'Paid',
                     }"
-                    ></div>
-                </div>
+            ></div>
+        </div>
 
-                {{-- Vertical Divider --}}
-                <div class="h-5 w-px rounded-full bg-hurricane/10"></div>
+        {{-- Vertical Divider --}}
+        <div
+            class="hidden h-5 w-px rounded-full bg-hurricane/10 min-[1170px]:block"
+        ></div>
 
-                {{-- Dark Mode Toggle --}}
-                <div
-                    x-on:click="features.dark_mode = ! features.dark_mode"
-                    class="flex cursor-pointer select-none items-center gap-3 rounded-full py-3 pl-4 pr-6 transition duration-200 hover:shadow-lg hover:shadow-black/[0.02]"
-                    :class="{
+        {{-- Dark Mode Toggle --}}
+        <div
+            x-on:click="features.dark_mode = ! features.dark_mode"
+            class="flex cursor-pointer select-none items-center gap-3 rounded-full py-3 pl-4 pr-6 transition duration-200 hover:shadow-lg hover:shadow-black/[0.01]"
+            :class="{
                         'bg-fair-pink text-salmon': features.dark_mode,
                         'bg-white text-dolphin': ! features.dark_mode,
                     }"
+        >
+            <div class="text-salmon">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="19"
+                    height="19"
+                    viewBox="0 0 24 24"
                 >
-                    <div class="text-salmon">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="19"
-                            height="19"
-                            viewBox="0 0 24 24"
-                        >
-                            <g fill="currentColor">
-                                <path
-                                    d="M19.9 2.307a.483.483 0 0 0-.9 0l-.43 1.095a.484.484 0 0 1-.272.274l-1.091.432a.486.486 0 0 0 0 .903l1.091.432a.48.48 0 0 1 .272.273L19 6.81c.162.41.74.41.9 0l.43-1.095a.484.484 0 0 1 .273-.273l1.091-.432a.486.486 0 0 0 0-.903l-1.091-.432a.484.484 0 0 1-.273-.274l-.43-1.095ZM16.033 8.13a.483.483 0 0 0-.9 0l-.157.399a.484.484 0 0 1-.272.273l-.398.158a.486.486 0 0 0 0 .903l.398.157c.125.05.223.148.272.274l.157.399c.161.41.739.41.9 0l.157-.4a.484.484 0 0 1 .272-.273l.398-.157a.486.486 0 0 0 0-.903l-.398-.158a.484.484 0 0 1-.272-.273l-.157-.4Z"
-                                />
-                                <path
-                                    d="M12 22c5.523 0 10-4.477 10-10c0-.463-.694-.54-.933-.143a6.5 6.5 0 1 1-8.924-8.924C12.54 2.693 12.463 2 12 2C6.477 2 2 6.477 2 12s4.477 10 10 10Z"
-                                />
-                            </g>
-                        </svg>
-                    </div>
-                    <div
-                        class="text-sm"
-                        :class="{
+                    <g fill="currentColor">
+                        <path
+                            d="M19.9 2.307a.483.483 0 0 0-.9 0l-.43 1.095a.484.484 0 0 1-.272.274l-1.091.432a.486.486 0 0 0 0 .903l1.091.432a.48.48 0 0 1 .272.273L19 6.81c.162.41.74.41.9 0l.43-1.095a.484.484 0 0 1 .273-.273l1.091-.432a.486.486 0 0 0 0-.903l-1.091-.432a.484.484 0 0 1-.273-.274l-.43-1.095ZM16.033 8.13a.483.483 0 0 0-.9 0l-.157.399a.484.484 0 0 1-.272.273l-.398.158a.486.486 0 0 0 0 .903l.398.157c.125.05.223.148.272.274l.157.399c.161.41.739.41.9 0l.157-.4a.484.484 0 0 1 .272-.273l.398-.157a.486.486 0 0 0 0-.903l-.398-.158a.484.484 0 0 1-.272-.273l-.157-.4Z"
+                        />
+                        <path
+                            d="M12 22c5.523 0 10-4.477 10-10c0-.463-.694-.54-.933-.143a6.5 6.5 0 1 1-8.924-8.924C12.54 2.693 12.463 2 12 2C6.477 2 2 6.477 2 12s4.477 10 10 10Z"
+                        />
+                    </g>
+                </svg>
+            </div>
+            <div
+                class="text-sm"
+                :class="{
                             'opacity-70': ! features.dark_mode,
                         }"
-                    >
-                        Dark mode
-                    </div>
-                </div>
+            >
+                Dark mode
+            </div>
+        </div>
 
-                {{-- Vertical Divider --}}
-                <div class="h-5 w-px rounded-full bg-hurricane/10"></div>
+        {{-- Vertical Divider --}}
+        <div
+            class="hidden h-5 w-px rounded-full bg-hurricane/10 min-[1170px]:block"
+        ></div>
 
-                {{-- Multi Language Toggle --}}
-                <div
-                    x-on:click="features.multi_language = ! features.multi_language"
-                    class="flex cursor-pointer select-none items-center gap-3 rounded-full py-3 pl-4 pr-6 transition duration-200 hover:shadow-lg hover:shadow-black/[0.02]"
-                    :class="{
-                        'bg-fair-pink text-salmon': features.multi_language,
-                        'bg-white text-dolphin': ! features.multi_language,
-                    }"
+        {{-- Multi Language Toggle --}}
+        <div
+            x-on:click="features.multi_language = ! features.multi_language"
+            class="flex cursor-pointer select-none items-center gap-3 rounded-full py-3 pl-4 pr-6 transition duration-200 hover:shadow-lg hover:shadow-black/[0.01]"
+            :class="{
+                'bg-fair-pink text-salmon': features.multi_language,
+                'bg-white text-dolphin': ! features.multi_language,
+            }"
+        >
+            <div class="text-salmon">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="19"
+                    height="19"
+                    viewBox="0 0 20 20"
                 >
-                    <div class="text-salmon">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="19"
-                            height="19"
-                            viewBox="0 0 20 20"
-                        >
-                            <g fill="currentColor">
-                                <path
-                                    d="M7.75 2.75a.75.75 0 0 0-1.5 0v1.258a32.987 32.987 0 0 0-3.599.278a.75.75 0 1 0 .198 1.487A31.545 31.545 0 0 1 8.7 5.545A19.381 19.381 0 0 1 7 9.56a19.418 19.418 0 0 1-1.002-2.05a.75.75 0 0 0-1.384.577a20.935 20.935 0 0 0 1.492 2.91a19.613 19.613 0 0 1-3.828 4.154a.75.75 0 1 0 .945 1.164A21.116 21.116 0 0 0 7 12.331c.095.132.192.262.29.391a.75.75 0 0 0 1.194-.91a18.97 18.97 0 0 1-.59-.815a20.888 20.888 0 0 0 2.333-5.332c.31.031.618.068.924.108a.75.75 0 0 0 .198-1.487a32.832 32.832 0 0 0-3.599-.278V2.75Z"
-                                />
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M13 8a.75.75 0 0 1 .671.415l4.25 8.5a.75.75 0 1 1-1.342.67L15.787 16h-5.573l-.793 1.585a.75.75 0 1 1-1.342-.67l4.25-8.5A.75.75 0 0 1 13 8Zm2.037 6.5L13 10.427L10.964 14.5h4.073Z"
-                                    clip-rule="evenodd"
-                                />
-                            </g>
-                        </svg>
-                    </div>
-                    <div
-                        class="text-sm"
-                        :class="{
-                            'opacity-70': ! features.multi_language,
-                        }"
-                    >
-                        Multi language
-                    </div>
-                </div>
+                    <g fill="currentColor">
+                        <path
+                            d="M7.75 2.75a.75.75 0 0 0-1.5 0v1.258a32.987 32.987 0 0 0-3.599.278a.75.75 0 1 0 .198 1.487A31.545 31.545 0 0 1 8.7 5.545A19.381 19.381 0 0 1 7 9.56a19.418 19.418 0 0 1-1.002-2.05a.75.75 0 0 0-1.384.577a20.935 20.935 0 0 0 1.492 2.91a19.613 19.613 0 0 1-3.828 4.154a.75.75 0 1 0 .945 1.164A21.116 21.116 0 0 0 7 12.331c.095.132.192.262.29.391a.75.75 0 0 0 1.194-.91a18.97 18.97 0 0 1-.59-.815a20.888 20.888 0 0 0 2.333-5.332c.31.031.618.068.924.108a.75.75 0 0 0 .198-1.487a32.832 32.832 0 0 0-3.599-.278V2.75Z"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            d="M13 8a.75.75 0 0 1 .671.415l4.25 8.5a.75.75 0 1 1-1.342.67L15.787 16h-5.573l-.793 1.585a.75.75 0 1 1-1.342-.67l4.25-8.5A.75.75 0 0 1 13 8Zm2.037 6.5L13 10.427L10.964 14.5h4.073Z"
+                            clip-rule="evenodd"
+                        />
+                    </g>
+                </svg>
+            </div>
+            <div
+                class="text-sm"
+                :class="{
+                    'opacity-70': ! features.multi_language,
+                }"
+            >
+                Multi language
             </div>
         </div>
     </div>
