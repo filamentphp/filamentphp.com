a9ef5f6998335fd85e3910e6ce347c4c9c090283
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-07-09 13:26:20 +0330

    Add creator to plugins page

--- resources/views/plugins/view-plugin.blade.php
+++ resources/views/plugins/view-plugin.blade.php
@@ -1,3 +1,10 @@
+@php
+    $formatter = new \NumberFormatter(
+        app()->getLocale(),
+        \NumberFormatter::PADDING_POSITION,
+    );
+@endphp
+
 <x-layouts.app>
     <section class="mx-auto w-full max-w-8xl px-5 sm:px-10">
         <div class="flex flex-wrap items-center justify-between gap-5 pt-20">
@@ -19,7 +26,7 @@
                 </svg>
                 <div class="text-xl font-medium">Plugins</div>
             </a>
-            <div class="flex flex-wrap items-center gap-4">
+            <div class="flex flex-wrap items-center gap-3">
                 {{-- Discord --}}
                 <a
                     href="{{ $plugin['discord_link'] }}"
@@ -67,7 +74,7 @@
                     <a
                         href="{{ $plugin['github_link'] }}"
                         target="_blank"
-                        class="block select-none rounded-bl-lg rounded-br-2xl rounded-tl-lg rounded-tr-lg bg-salmon px-10 py-2.5 text-center text-sm font-medium text-white shadow-xl shadow-black/[0.02] transition duration-300 hover:-translate-y-0.5 hover:bg-salmon/80"
+                        class="block select-none rounded-bl-lg rounded-br-2xl rounded-tl-lg rounded-tr-lg bg-salmon py-2.5 pl-5 pr-6 text-center text-sm font-medium text-white shadow-xl shadow-black/[0.02] transition duration-300 hover:-translate-y-0.5 hover:bg-salmon/80"
                     >
                         Visit Github
                     </a>
@@ -76,12 +83,175 @@
                     <a
                         href="{{ $plugin['buy_link'] }}"
                         target="_blank"
-                        class="block select-none rounded-bl-lg rounded-br-2xl rounded-tl-lg rounded-tr-lg bg-salmon px-10 py-2.5 text-center text-sm font-medium text-white shadow-xl shadow-black/[0.02] transition duration-300 hover:-translate-y-0.5 hover:bg-salmon/80"
+                        class="block select-none rounded-bl-lg rounded-br-2xl rounded-tl-lg rounded-tr-lg bg-salmon px-6 py-2.5 text-center text-sm font-medium text-white shadow-xl shadow-black/[0.02] transition duration-300 hover:-translate-y-0.5 hover:bg-salmon/80"
                     >
                         Buy for
                         {{ $plugin['price'] }}
                     </a>
                 @endif
+            </div>
+        </div>
+
+        <div class="flex flex-col items-start gap-20 pt-7 xl:flex-row">
+            {{-- Left Side --}}
+            <div class="w-full">
+                <div class="">
+                    <div class="text-3xl font-extrabold">
+                        {{ $plugin['name'] }}
+                    </div>
+                    <div class="pt-2 font-medium text-dolphin/80">
+                        {{ $plugin['description'] }}
+                    </div>
+                </div>
+            </div>
+
+            {{-- Right Side --}}
+            <div class="w-full max-w-md">
+                {{-- Creator --}}
+                <div class="pt-10 text-evening">
+                    <div class="text-lg font-extrabold">Creator</div>
+                    <div class="grid w-full place-items-center pt-2">
+                        {{-- Avatar --}}
+                        <div
+                            class="h-24 w-24 shrink-0 overflow-hidden rounded-full"
+                        >
+                            @if ($plugin['author']['avatar'])
+                                <img
+                                    src="{{ $plugin['author']['avatar'] }}"
+                                    alt="plugin.author.name"
+                                    class="aspect-square h-full w-full"
+                                />
+                            @else
+                                <div
+                                    class="grid h-full w-full place-items-center bg-fair-pink text-salmon/50"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="22"
+                                        height="22"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <g fill="currentColor">
+                                            <circle
+                                                cx="12"
+                                                cy="6"
+                                                r="4"
+                                            />
+                                            <path
+                                                d="M20 17.5c0 2.485 0 4.5-8 4.5s-8-2.015-8-4.5S7.582 13 12 13s8 2.015 8 4.5Z"
+                                            />
+                                        </g>
+                                    </svg>
+                                </div>
+                            @endif
+                        </div>
+
+                        {{-- Name --}}
+                        <div class="pt-3.5 text-lg font-bold">
+                            {{ $plugin['author']['name'] }}
+                        </div>
+
+                        {{-- Social Links --}}
+                        @if ($plugin['author']['twitter'] || $plugin['author']['github'])
+                            <div class="flex items-center gap-3.5 pt-2">
+                                {{-- Twitter --}}
+                                @if ($plugin['author']['twitter'])
+                                    <a
+                                        target="_blank"
+                                        href="{{ $plugin['author']['twitter'] }}"
+                                        class="grid h-8 w-8 place-items-center rounded-full bg-merino text-hurricane transition duration-200 hover:scale-110 hover:text-salmon"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                fill="currentColor"
+                                                fill-rule="evenodd"
+                                                d="M15.021 3.343c.509-.087 1.078-.116 1.614-.025a4.85 4.85 0 0 1 2.54 1.273c.456.01.905-.08 1.302-.208a5.36 5.36 0 0 0 1.098-.501l.009-.006a.75.75 0 0 1 1.042 1.037c-.207.315-.496.877-.819 1.507l-.155.301c-.185.36-.375.724-.552 1.036c-.111.196-.23.395-.35.567v.274A12.34 12.34 0 0 1 8.287 21.03a12.32 12.32 0 0 1-6.694-1.97a.75.75 0 0 1 .5-1.374a7.471 7.471 0 0 0 4.033-.642a4.858 4.858 0 0 1-2.61-2.922a.75.75 0 0 1 .147-.722l.01-.01A4.848 4.848 0 0 1 2.05 9.793v-.052a.75.75 0 0 1 .553-.724A4.84 4.84 0 0 1 2.09 6.84a4.9 4.9 0 0 1 .65-2.442a.75.75 0 0 1 1.232-.1a10.89 10.89 0 0 0 7.006 3.93a4.85 4.85 0 0 1 2.562-4.406c.402-.214.934-.385 1.482-.479ZM3.743 10.891a3.35 3.35 0 0 0 2.503 2.164a.75.75 0 0 1 .072 1.453c-.272.083-.551.14-.834.173a3.358 3.358 0 0 0 2.59 1.3a.75.75 0 0 1 .45 1.339a8.97 8.97 0 0 1-3.548 1.695a10.82 10.82 0 0 0 3.313.515h.009A10.838 10.838 0 0 0 19.25 8.607v-.535a.75.75 0 0 1 .186-.495c.07-.079.19-.261.36-.56c.16-.282.338-.622.523-.981l.033-.066a4.992 4.992 0 0 1-1.593.097a.75.75 0 0 1-.47-.237a3.35 3.35 0 0 0-1.904-1.032a3.42 3.42 0 0 0-1.11.025a3.605 3.605 0 0 0-1.028.323a3.35 3.35 0 0 0-1.678 3.74a.75.75 0 0 1-.767.925a12.39 12.39 0 0 1-8.149-3.627a3.41 3.41 0 0 0-.063.657v.002a3.34 3.34 0 0 0 1.486 2.785A.75.75 0 0 1 4.64 11a4.798 4.798 0 0 1-.897-.11Z"
+                                                clip-rule="evenodd"
+                                            />
+                                        </svg>
+                                    </a>
+                                @endif
+
+                                {{-- Github --}}
+                                @if ($plugin['author']['github'])
+                                    <a
+                                        target="_blank"
+                                        href="{{ $plugin['author']['github'] }}"
+                                        class="grid h-8 w-8 place-items-center rounded-full bg-merino text-hurricane transition duration-200 hover:scale-110 hover:text-salmon"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="22"
+                                            height="22"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <g
+                                                fill="none"
+                                                stroke="currentColor"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="1.5"
+                                            >
+                                                <path
+                                                    d="M16 22.027v-2.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 0 0-1.5-3.75a5.07 5.07 0 0 0-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 0 0-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 0 0 5 5.797a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.58v2.87"
+                                                />
+                                                <path
+                                                    d="M9 20.027c-3 .973-5.5 0-7-3"
+                                                />
+                                            </g>
+                                        </svg>
+                                    </a>
+                                @endif
+                            </div>
+                        @endif
+
+                        {{-- Stats --}}
+                        <div
+                            class="mt-3 grid grid-cols-3 gap-10 rounded-2xl bg-white px-6 pb-3.5 pt-3 text-center shadow-lg shadow-black/[0.01]"
+                        >
+                            {{-- Plugins --}}
+                            <div class="space-y-0.5">
+                                <div class="font-extrabold">
+                                    {{ $formatter->format($plugin['author']['stats']['plugins']) }}
+                                </div>
+                                <div
+                                    class="text-sm font-medium text-dolphin/80"
+                                >
+                                    Plugins
+                                </div>
+                            </div>
+
+                            {{-- Views --}}
+                            <div class="space-y-0.5">
+                                <div class="font-extrabold">
+                                    {{ $formatter->format($plugin['author']['stats']['views']) }}
+                                </div>
+                                <div
+                                    class="text-sm font-medium text-dolphin/80"
+                                >
+                                    Views
+                                </div>
+                            </div>
+
+                            {{-- Stars --}}
+                            <div class="space-y-0.5">
+                                <div class="font-extrabold">
+                                    {{ $formatter->format($plugin['author']['stats']['stars']) }}
+                                </div>
+                                <div
+                                    class="text-sm font-medium text-dolphin/80"
+                                >
+                                    Stars
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
