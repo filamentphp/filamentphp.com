fe2c97c2123bbd275f1c1b7909e302dc089744ff
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-07-30 21:42:07 +0330

    Add star animation

--- app/Livewire/StarRecord.php
+++ app/Livewire/StarRecord.php
@@ -11,6 +11,16 @@ class Starrecord extends Component
 {
     public Model $record;
 
+    public $isStarred;
+
+    public $starsCount;
+
+    public function mount()
+    {
+        $this->isStarred = $this->isStarred();
+        $this->starsCount = $this->record->getStarsCount();
+    }
+
     public function isStarred(): bool
     {
         return $this->record->stars()->where('ip', request()->ip())->exists();
@@ -21,10 +31,16 @@ class Starrecord extends Component
         if ($this->isStarred()) {
             app(Unstar::class)($this->record);
 
+            $this->starsCount = $this->record->getStarsCount();
+            $this->isStarred = false;
+
             return;
         }
 
         app(Star::class)($this->record);
+
+        $this->starsCount = $this->record->getStarsCount();
+        $this->isStarred = true;
     }
 
     public function render()
