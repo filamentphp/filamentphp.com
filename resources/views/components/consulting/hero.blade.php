54d7a1cdae9ad896cde25eb35fbc6e42252a0feb
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-07-30 15:18:24 +0330

    Add consulting hero

---
+++ resources/views/components/consulting/hero.blade.php
@@ -0,0 +1,35 @@
+<section
+    x-cloak
+    x-data="{}"
+    class="mx-auto w-full max-w-6xl px-10 pt-20"
+>
+    {{-- Header --}}
+    <div class="flex items-center justify-around gap-20">
+        <div>
+            {{-- Title --}}
+            <div
+                class="text-5xl font-black"
+                x-ref="consulting"
+            >
+                Consulting
+            </div>
+            {{-- Message --}}
+            <div
+                x-ref="message"
+                class="max-w-lg pt-10 text-lg font-medium text-dolphin"
+            >
+                If you're looking for dedicated help with your Filament project,
+                we're here for you. Schedule a consulting call with one of our
+                team members.
+            </div>
+        </div>
+
+        {{-- Phone Illustration --}}
+        <img
+            x-ref="phone"
+            src="{{ Vite::asset('resources/images/consulting/phone.webp') }}"
+            alt=""
+            class="block w-60"
+        />
+    </div>
+</section>
