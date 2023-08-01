642d5d08f73efd2b53d37f8453e6362f8a4ea09d
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2023-08-01 15:31:02 +0330

    Use accordion panel for plugin categories

--- resources/views/components/plugins/list.blade.php
+++ resources/views/components/plugins/list.blade.php
@@ -41,6 +41,7 @@
     x-data="{
         searchEngine: null,
         search: '',
+        showCategories: false,
         selectedCategories: new Set(),
         selectedVersion: '3',
         selectedPrice: 'All',
@@ -357,8 +358,52 @@
     <div class="items-start gap-5 pt-5 sm:flex">
         {{-- Categories --}}
         <div class="w-full sm:max-w-[15rem]">
-            <div class="font-semibold">Categories</div>
-            <div class="flex flex-wrap gap-x-2 gap-y-0.5 pt-5 sm:block">
+            <div class="hidden font-semibold sm:block">Categories</div>
+            <div
+                class="flex cursor-pointer justify-between gap-5 rounded-lg bg-merino px-3.5 py-2.5 sm:hidden"
+                x-on:click="showCategories = !showCategories"
+            >
+                <div class="font-semibold">
+                    <span class="">Select Categories</span>
+                    <span class="text-sm tracking-tighter">
+                        <span>(</span>
+                        <span x-text="selectedCategories.size"></span>
+                        <span>)</span>
+                    </span>
+                </div>
+                <div
+                    class="transition duration-300"
+                    :class="{
+                        'rotate-180': !showCategories,
+                    }"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                    >
+                        <g
+                            fill="none"
+                            fill-rule="evenodd"
+                        >
+                            <path
+                                d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"
+                            />
+                            <path
+                                fill="currentColor"
+                                d="M12.707 15.707a1 1 0 0 1-1.414 0L5.636 10.05A1 1 0 1 1 7.05 8.636l4.95 4.95l4.95-4.95a1 1 0 0 1 1.414 1.414l-5.657 5.657Z"
+                            />
+                        </g>
+                    </svg>
+                </div>
+            </div>
+            <div
+                class="flex flex-wrap gap-x-2 gap-y-0.5 pt-5"
+                :class="{
+                    'opacity-0 invisible h-0 sm:opacity-100 sm:visible sm:h-auto': !showCategories,
+                }"
+            >
                 @foreach ($categories as $category)
                     <x-plugins.category
                         :name="$category->name"
