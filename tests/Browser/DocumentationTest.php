e6a7257aea5aa5e0e71b9ae724f9c5c02194647d
Christopher Pitt <cgpitt@gmail.com>
2023-08-20 19:10:09 +0200

    Add a test for switching documentation versions
    This ensures that the documentation version switcher continues to work.

--- tests/Browser/DocumentationTest.php
+++ tests/Browser/DocumentationTest.php
@@ -19,4 +19,26 @@ class DocumentationTest extends DuskTestCase
                 ->assertSee('Filament Documentation')
         );
     }
+
+    /**
+     * @throws Throwable
+     */
+    public function testDocumentationCanSwitchToDifferentVersions(): void
+    {
+        $this->browse(
+            fn (Browser $browser) => $browser
+                ->visitRoute('docs')
+
+                // has and can open click on the version selector
+                ->waitFor("[aria-label='Open version options']")
+                ->click("[aria-label='Open version options']")
+
+                // version 2.x option becomes visible and can be clicked
+                ->assertVisible("[href*='2.x']")
+                ->clickAndWaitForReload("[href*='2.x']")
+
+                // will be redirected to 2.x documentation
+                ->assertSee('This package is compatible with other Filament v2.x products')
+        );
+    }
 }
