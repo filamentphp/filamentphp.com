2ae5dd661e702dc515cc237ce34acf8c1ade3ed5
Dan Harrin <git@danharrin.com>
2023-10-28 22:14:49 +0100

    wip

--- docs/src/scripts/clipboard.js
+++ docs/src/scripts/clipboard.js
@@ -1,7 +1,7 @@
 import ClipboardJS from 'clipboard/dist/clipboard'
 
 export default (Alpine) => {
-    Alpine.data('clipboard', () => ({
+    Alpine.data('codeBlockClipboard', () => ({
         codeBlocks: document.querySelectorAll('pre') || [],
         clipboardIcon: `<svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path><path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"></path></svg>`,
         clipboardCopiedIcon: `<svg fill="currentColor" class="fill-current text-white h-5 w-5" viewBox="0 0 20 20"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>`,
@@ -15,28 +15,28 @@ export default (Alpine) => {
 
                 wrapper.appendChild(element);
 
-                let copyToClipboardBtn = document.createElement('button');
-                copyToClipboardBtn.innerHTML = this.clipboardIcon;
-                copyToClipboardBtn.id = `clipButton-${key}`;
+                let copyToClipboardButton = document.createElement('button');
+                copyToClipboardButton.innerHTML = this.clipboardIcon;
+                copyToClipboardButton.id = `code-block-copy-button-${key}`;
 
-                ['md:block', 'hidden'].forEach((value) => copyToClipboardBtn.classList.add(value));
+                ['md:block', 'hidden'].forEach((value) => copyToClipboardButton.classList.add(value));
 
-                copyToClipboardBtn.setAttribute('aria-label', 'Copy to Clipboard');
-                copyToClipboardBtn.setAttribute('title', 'Copy to Clipboard');
-                copyToClipboardBtn.classList.add('copyBtn');
+                copyToClipboardButton.setAttribute('aria-label', 'Copy to Clipboard');
+                copyToClipboardButton.setAttribute('title', 'Copy to Clipboard');
+                copyToClipboardButton.classList.add('code-block-copy-button');
 
-                wrapper.appendChild(copyToClipboardBtn);
+                wrapper.appendChild(copyToClipboardButton);
 
-                let copyToClipboard = new ClipboardJS(`#${copyToClipboardBtn.id}`);
+                let copyToClipboard = new ClipboardJS(`#${copyToClipboardButton.id}`);
                 copyToClipboard.on('success', (element) => {
-                    copyToClipboardBtn.innerHTML = this.clipboardCopiedIcon;
+                    copyToClipboardButton.innerHTML = this.clipboardCopiedIcon;
                     element.clearSelection();
-                    setTimeout(() => copyToClipboardBtn.innerHTML = this.clipboardIcon, 1500);
+                    setTimeout(() => copyToClipboardButton.innerHTML = this.clipboardIcon, 1500);
                 });
 
                 let codeElement = element.querySelector('code');
-                codeElement.id = `clipText-${key}`;
-                copyToClipboardBtn.dataset.clipboardTarget = `#${codeElement.id}`;
+                codeElement.id = `code-block-text-${key}`;
+                copyToClipboardButton.dataset.clipboardTarget = `#${codeElement.id}`;
             });
         },
     }))
