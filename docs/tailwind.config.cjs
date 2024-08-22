ababb35307133963cdaa069f06346e4f3cfa8bd1
Dan Harrin <git@danharrin.com>
2024-08-22 13:10:10 +0100

    add components and rebuild

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -50,5 +50,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
