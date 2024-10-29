311fb98f8c4ef9470bc9ff46c6042b67e7820ef4
atmonshi <atmonshi@users.noreply.github.com>
2024-10-29 11:54:19 +0000

    chore: fix code style

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\PluginController;
 use Illuminate\Support\Facades\Route;
 
