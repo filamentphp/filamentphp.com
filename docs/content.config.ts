dd26913aa46a5bb53e9ccf8610b56cce9fbade3c
danharrin <danharrin@users.noreply.github.com>
2025-03-08 08:00:40 +0000

    chore: fix code style

--- docs/content.config.ts
+++ docs/content.config.ts
@@ -4,9 +4,12 @@ import { docsSchema } from '@astrojs/starlight/schema'
 import { glob } from 'astro/loaders'
 
 export const collections = {
-	docs: defineCollection({ loader: glob({
-        pattern: `**/[^_]*.{md,mdx}`,
-        base: './docs/content/docs',
-        generateId: ({ entry }) => entry.split('.').slice(0, -1).join('.'),
-    }), schema: docsSchema() }),
+    docs: defineCollection({
+        loader: glob({
+            pattern: `**/[^_]*.{md,mdx}`,
+            base: './docs/content/docs',
+            generateId: ({ entry }) => entry.split('.').slice(0, -1).join('.'),
+        }),
+        schema: docsSchema(),
+    }),
 }
